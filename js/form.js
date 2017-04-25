3a23f6d5b8dfb13635edecfc8535c35f838c0963
Guilherme Zabot <zabot.gui@gmail.com>
2017-04-25 17:43:19 -0300

    Requisicao com ajax

--- js/form.js
+++ js/form.js
@@ -3,10 +3,7 @@ botaoAdicionar.addEventListener("click", function(event) {
   event.preventDefault();
 
   var form = document.querySelector("#form-adiciona");
-
   var paciente = obtemPacienteDoFormulario(form);
-
-  var pacienteTr = montaTr(paciente);
 
   var erros = validaPaciente(paciente);
   console.log(erros);
@@ -16,14 +13,18 @@ botaoAdicionar.addEventListener("click", function(event) {
     return;
   }
 
-  var tabela = document.querySelector("#tabela-pacientes");
-
-  tabela.appendChild(pacienteTr);
+  adicionaPacienteNaTabela(paciente);
 
   form.reset();
   var mensagensErro = document.querySelector("#mensagens-erro");
   mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+  var pacienteTr = montaTr(paciente);
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros){
   var ul = document.querySelector("#mensagens-erro");
